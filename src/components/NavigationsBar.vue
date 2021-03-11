059962d2371f86098dd04ddfaf886f672f370240
Mads Hansen Lund <mhl@nine.dk>
2021-03-11 14:37:55 +0100

    add model

--- src/components/NavigationsBar.vue
+++ src/components/NavigationsBar.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
     <v-tabs v-model="tab">
-      <v-tab v-for="item in items" :key="item" >{{item}}</v-tab>
+      <v-tab v-for="item in items" :key="item.name" >{{item.name}}</v-tab>
     </v-tabs>
 
     FUUUCK §{{tab}}
 
-
     <v-tabs-items v-model="tab">
       <v-tab-item
           v-for="item in items"
-          :key="item"
+          :key="item.name"
       >
         <v-card
             color="basil"
             flat
         >
-          <v-card-text>{{ item }}</v-card-text>
+          <v-card-text>{{ item.name }} -> {{item.url}}</v-card-text>
         </v-card>
       </v-tab-item>
     </v-tabs-items>
@@ -29,12 +28,17 @@
 import Component from "vue-class-component";
 import Vue from "vue";
 import CvrInput from "@/components/CvrInput.vue";
+import {SvigTab} from "@/model/SvigTab";
 
 @Component({
   components: { CvrInput }
 })
 export default class NavigationsBar extends Vue {
   tab = null;
-  items = ["Fasteomkostninger", "Løn", "Svig"];
+  items = [
+    new SvigTab("Fasteomkostninger", "http://shit1"),
+    new SvigTab("Løn", "http://shit2"),
+    new SvigTab("svig", "http://shit3"),
+  ];
 }
 </script>
